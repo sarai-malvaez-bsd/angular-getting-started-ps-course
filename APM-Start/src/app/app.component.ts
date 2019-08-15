677766e3b8f93035b2b68e659deb4177188631fe
saraibsd2019 <sarammbsd@outlook.com>
2019-08-15 11:21:01 -0500

    The Firts Time Proyect

---
+++ APM-Start/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:"app-component",
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+
+}) // Component
+
+export class AppComponent {
+  pageTitle: 'Acme Product Managment';
+} // AppComponent
+
+
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'pm-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Angular: Getting Started';
+// }
---
+++ APM-Start/src/app/productsList/productList.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:"pm-product-list",
+  templateUrl: './productList.component.html',
+  styleUrls: ['./productList.component.css']
+}) // Component
+
+export class ProductListComponent {
+
+} // ProductListComponent
